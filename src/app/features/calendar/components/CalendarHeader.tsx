4bf8d63ab9fac3efccbd0f99f5ed349d274f767b
Gosuke <ogawa.gousuke@gmail.com>
2025-06-04 19:30:15 +0900

    分割年表示を実装

--- src/app/features/calendar/components/CalendarHeader.tsx
+++ src/app/features/calendar/components/CalendarHeader.tsx
@@ -6,13 +6,33 @@ interface CalendarHeaderProps {
 }
 
 const renderMonthCell = (days: dayjs.Dayjs[]) => {
+  const year = days[0].format('YYYY');
   const month = days[0].format('M');
+  const monthOfLastDay = days[days.length - 1].format('M');
+  var colsForFirstItem = 7;
+
+  if (month !== monthOfLastDay) {
+    colsForFirstItem = days.reduce((acc, day) => {
+      if (day.format('M') === month) {
+        return acc + 1;
+      }
+      return acc;
+    }, 0);
+  }
+
+  const colsForLastItem = 7 - colsForFirstItem;
+
   return (
     <tr>
         <th rowSpan={2} className="align-middle">日時</th>
-        <th colSpan={7}>2026年{month}月</th>
+        <th colSpan={colsForFirstItem}>{colsForFirstItem > 1 ? year + '年' : ''}{month}月</th>
+        {colsForLastItem > 0 ?
+        <th colSpan={colsForLastItem}>{colsForLastItem > 1 ? year + '年' : ''}{monthOfLastDay}月</th>
+        : null}
     </tr>
   );
+
+  
 }
 
 const renderDay = (day: dayjs.Dayjs) => {
