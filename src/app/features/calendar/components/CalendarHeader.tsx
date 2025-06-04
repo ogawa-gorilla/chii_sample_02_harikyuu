9c09afcca6afda40fd6f8ca333582c6463b6c390
Gosuke <ogawa.gousuke@gmail.com>
2025-06-04 19:19:19 +0900

    いったん月表示を実装、細かい分割はできない

--- src/app/features/calendar/components/CalendarHeader.tsx
+++ src/app/features/calendar/components/CalendarHeader.tsx
@@ -5,23 +5,35 @@ interface CalendarHeaderProps {
   days: dayjs.Dayjs[];
 }
 
-export default function CalendarHeader({ days }: CalendarHeaderProps) {
+const renderMonthCell = (days: dayjs.Dayjs[]) => {
+  const month = days[0].format('M');
+  return (
+    <tr>
+        <th rowSpan={2} className="align-middle">日時</th>
+        <th colSpan={7}>2026年{month}月</th>
+    </tr>
+  );
+}
+
+const renderDay = (day: dayjs.Dayjs) => {
+  const isSaturday = day.format('d') === '6';
+  const isSunday = day.format('d') === '0';
+  const className = isSaturday ? 'saturday' : isSunday ? 'sunday' : '';
+  return (
+    <th key={day.format()} className={className}>
+      {day.format('D')}<br />({day.locale('ja').format('ddd')})
+    </th>
+  );
+}
+
+export default function CalendarHeader({ days }: CalendarHeaderProps) {;
+
   return (
     <thead>
-      <tr>
-        <th rowSpan={2} className="align-middle">日時</th>
-        <th colSpan={7}>2026年6月</th>
-      </tr>
+      {renderMonthCell(days)}
       <tr>
         {days.map((day) => {
-          const isSaturday = day.format('d') === '6';
-          const isSunday = day.format('d') === '0';
-          const className = isSaturday ? 'saturday' : isSunday ? 'sunday' : '';
-          return (
-            <th key={day.format()} className={className}>
-              {day.format('D')}<br />({day.locale('ja').format('ddd')})
-            </th>
-          );
+          return renderDay(day);
         })}
       </tr>
     </thead>
