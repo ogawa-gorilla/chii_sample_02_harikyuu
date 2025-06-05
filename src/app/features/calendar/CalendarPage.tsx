aef7feed0fd7ca562e08c596d298adcc389f4d05
Gosuke <ogawa.gousuke@gmail.com>
2025-06-05 19:55:35 +0900

    文言修正

--- src/app/features/calendar/CalendarPage.tsx
+++ src/app/features/calendar/CalendarPage.tsx
@@ -5,7 +5,7 @@ import Calendar from "./components/Calendar";
 export default function CalendarPage() {
   return (
     <Container fluid className="py-3">
-    <h5 className="text-center mb-3">予約カレンダー（店長用）</h5>
+    <h5 className="text-center mb-3">予約カレンダー</h5>
     
 
     <Calendar />
