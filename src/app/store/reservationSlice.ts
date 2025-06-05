44a005ed4bae60aafb3ca2ff135d1a510b93d949
Gosuke <ogawa.gousuke@gmail.com>
2025-06-05 19:35:45 +0900

    テストデータを投入

--- src/app/store/reservationSlice.ts
+++ src/app/store/reservationSlice.ts
@@ -39,6 +39,134 @@ const initialState: ReservationState = {
         password: 'suzuki',
         themeColor: '#0A192F',
       }
+    },
+    {
+      id: 3,
+      date: '2025-05-28',
+      client: '田中 花子',
+      time: '10:00',
+      duration: 1,
+      status: '予約済',
+      staff: {
+        id: '2',
+        name: '佐藤',
+        email: 'sato@example.com',
+        role: Role.STAFF,
+        password: 'sato',
+        themeColor: '#2D0B5A',
+      }
+    },
+    {
+      id: 4,
+      date: '2025-06-04',
+      client: '松本 健太',
+      time: '14:00',
+      duration: 1,
+      status: '予約済',
+      staff: {
+        id: '3',
+        name: '山田',
+        email: 'yamada@example.com',
+        role: Role.STAFF,
+        password: 'yamada',
+        themeColor: '#4B000F',
+      }
+    },
+    {
+      id: 5,
+      date: '2025-06-04',
+      client: '中村 美咲',
+      time: '16:30',
+      duration: 1,
+      status: '予約済',
+      staff: {
+        id: '2',
+        name: '佐藤',
+        email: 'sato@example.com',
+        role: Role.STAFF,
+        password: 'sato',
+        themeColor: '#2D0B5A',
+      }
+    },
+    {
+      id: 6,
+      date: '2025-06-05',
+      client: '佐々木 正人',
+      time: '09:30',
+      duration: 1,
+      status: '予約済',
+      staff: {
+        id: '1',
+        name: '鈴木',
+        email: 'manager@example.com',
+        role: Role.MANAGER,
+        password: 'suzuki',
+        themeColor: '#0A192F',
+      }
+    },
+    {
+      id: 7,
+      date: '2025-06-06',
+      client: '吉田 雅子',
+      time: '13:00',
+      duration: 1,
+      status: '予約済',
+      staff: {
+        id: '3',
+        name: '山田',
+        email: 'yamada@example.com',
+        role: Role.STAFF,
+        password: 'yamada',
+        themeColor: '#4B000F',
+      }
+    },
+    {
+      id: 8,
+      date: '2025-06-07',
+      client: '高橋 昭彦',
+      time: '15:00',
+      duration: 1,
+      status: '予約済',
+      staff: {
+        id: '2',
+        name: '佐藤',
+        email: 'sato@example.com',
+        role: Role.STAFF,
+        password: 'sato',
+        themeColor: '#2D0B5A',
+      }
+    },
+    {
+      id: 9,
+      date: '2025-06-08',
+      client: '小林 千代',
+      time: '11:30',
+      duration: 1,
+      status: '予約済',
+      staff: {
+        id: '1',
+        name: '鈴木',
+        email: 'manager@example.com',
+        role: Role.MANAGER,
+        password: 'suzuki',
+        themeColor: '#0A192F',
+      }
+    },
+    {
+      id: 10,
+      date: '2025-06-11',
+      client: '伊藤 慎一',
+      time: '17:00',
+      duration: 1,
+      status: '予約済',
+      staff: {
+        id: '3',
+        name: '山田',
+        email: 'yamada@example.com',
+        role: Role.STAFF,
+        password: 'yamada',
+        themeColor: '#4B000F',
+      }
     }
   ]  
 }
