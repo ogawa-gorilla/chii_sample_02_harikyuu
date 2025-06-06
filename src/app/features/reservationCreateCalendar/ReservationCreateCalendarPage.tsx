ae96e503e50d34b9f44104f436c88a76439055ff
Gosuke <ogawa.gousuke@gmail.com>
2025-06-06 19:00:23 +0900

    カレンダーセル仮実装

--- src/app/features/reservationCreateCalendar/ReservationCreateCalendarPage.tsx
+++ src/app/features/reservationCreateCalendar/ReservationCreateCalendarPage.tsx
@@ -1,14 +1,23 @@
 import Calendar from "@/app/components/calendar";
+import { useAppSelector } from "@/app/hooks";
+import { Reservation } from "@/app/types/reservation";
 import { useState } from "react";
 import { Container } from "react-bootstrap";
+import ReservationCreateCalendarCell from "./components/ReservationCreateCalendarCell";
 import StaffSelector from "./components/StaffSelector";
 
 export default function ReservationCreateCalendarPage() {
 
   const [selectedStaff, setSelectedStaff] = useState<string>('none');
 
+  const allReservations = useAppSelector((state) => state.reservation.reservations).filter((reservation) => reservation.staff.id === selectedStaff);
+
   const handleStaffChange = (staff: string) => {
     setSelectedStaff(staff);
+  }
+
+  const handleCellClick = (date: string, hour: number, reservations: Reservation[]) => {
+    console.log(date, hour, reservations);
   }
 
   return (
@@ -16,7 +25,7 @@ export default function ReservationCreateCalendarPage() {
 
       <h5 className="text-center mb-3">予約作成</h5>
       <StaffSelector selectedStaff={selectedStaff} onStaffChange={handleStaffChange} />
-      { selectedStaff === 'none' ? <div></div> : <Calendar /> }
+      { selectedStaff === 'none' ? <div></div> : <Calendar cellComponent={ReservationCreateCalendarCell} cellProps={{ allReservations, onCellClick: handleCellClick }} /> }
   </Container>
   );
 }
---
+++ src/app/features/reservationCreateCalendar/components/ReservationCreateCalendarCell.tsx
@@ -0,0 +1,35 @@
+import { Reservation } from "@/app/types/reservation";
+import dayjs from "dayjs";
+
+interface ReservationCreateCalendarCellProps {
+  day: dayjs.Dayjs;
+  hour: number;
+  allReservations: Reservation[];
+  onCellClick?: (date: string, hour: number, reservations: Reservation[]) => void;
+}
+
+const getReservations = (reservations: Reservation[], day: dayjs.Dayjs, hour: number) => {
+  return reservations.filter(reservation => reservation.date === day.format('YYYY-MM-DD') && reservation.time === hour.toString() + ":00");
+}
+
+export default function ReservationCreateCalendarCell({ day, hour, allReservations, onCellClick }: ReservationCreateCalendarCellProps) {
+
+  
+  const hasReservations = getReservations(allReservations, day, hour).length > 0;
+
+  const handleClick = () => {}
+
+  return (
+    <td 
+      key={`${day.format()}-${hour}`} 
+      onClick={handleClick}
+    >
+      {hasReservations ? (
+        <div className="align-items-center">
+          ×
+        </div>
+      ) : 
+      <div>空</div>}
+    </td>
+  );
+}
