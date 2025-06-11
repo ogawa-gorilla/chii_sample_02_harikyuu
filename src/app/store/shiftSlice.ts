e877ef0c6277d3ed09a55a3eb9abe737e717b437
Gosuke <ogawa.gousuke@gmail.com>
2025-06-11 21:22:43 +0900

    　シフトテンプレート

--- src/app/store/shiftSlice.ts
+++ src/app/store/shiftSlice.ts
@@ -1,4 +1,4 @@
-import { Shift } from '@/app/types/shift'
+import { Shift, ShiftTemplate } from '@/app/types/shift'
 import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { SHIFT_TESTDATA } from '../components/testdata/shiftTestData'
 import { TemporalHoliday } from '../types/temporalHoliday'
@@ -7,11 +7,119 @@ import { RootState } from './store'
 interface ShiftState {
     shifts: Shift[]
     temporalHolidays: TemporalHoliday[]
+    shiftTemplates: ShiftTemplate[]
 }
 
 const initialState: ShiftState = {
     shifts: SHIFT_TESTDATA,
     temporalHolidays: [{ date: '2025-06-07', name: '店長出張' }],
+    shiftTemplates: [
+        // 店長は全部出勤
+        {
+            id: '1',
+            userId: '1',
+            configPerDay: [
+                [
+                    '0',
+                    {
+                        day: '0',
+                        times: [{ startTime: '09:00', endTime: '18:00' }],
+                    },
+                ], // 日
+                [
+                    '3',
+                    {
+                        day: '3',
+                        times: [{ startTime: '09:00', endTime: '18:00' }],
+                    },
+                ], // 水
+                [
+                    '4',
+                    {
+                        day: '4',
+                        times: [{ startTime: '09:00', endTime: '18:00' }],
+                    },
+                ], // 木
+                [
+                    '5',
+                    {
+                        day: '5',
+                        times: [{ startTime: '09:00', endTime: '18:00' }],
+                    },
+                ], // 金
+                [
+                    '6',
+                    {
+                        day: '6',
+                        times: [{ startTime: '09:00', endTime: '18:00' }],
+                    },
+                ], // 土
+            ],
+        },
+        // 佐藤：水金休み
+        {
+            id: '2',
+            userId: '2',
+            configPerDay: [
+                [
+                    '0',
+                    {
+                        day: '0',
+                        times: [{ startTime: '09:00', endTime: '18:00' }],
+                    },
+                ], // 日
+                [
+                    '4',
+                    {
+                        day: '4',
+                        times: [{ startTime: '09:00', endTime: '18:00' }],
+                    },
+                ], // 木
+                [
+                    '6',
+                    {
+                        day: '6',
+                        times: [{ startTime: '09:00', endTime: '18:00' }],
+                    },
+                ], // 土
+            ],
+        },
+        // 山田：木休み
+        {
+            id: '3',
+            userId: '3',
+            configPerDay: [
+                [
+                    '0',
+                    {
+                        day: '0',
+                        times: [{ startTime: '09:00', endTime: '18:00' }],
+                    },
+                ], // 日
+                [
+                    '3',
+                    {
+                        day: '3',
+                        times: [{ startTime: '09:00', endTime: '18:00' }],
+                    },
+                ], // 水
+                [
+                    '5',
+                    {
+                        day: '5',
+                        times: [{ startTime: '09:00', endTime: '18:00' }],
+                    },
+                ], // 金
+                [
+                    '6',
+                    {
+                        day: '6',
+                        times: [{ startTime: '09:00', endTime: '18:00' }],
+                    },
+                ], // 土
+            ],
+        },
+    ],
 }
 
 const shiftSlice = createSlice({
--- src/app/types/shift.ts
+++ src/app/types/shift.ts
@@ -1,7 +1,23 @@
 export interface Shift {
-  id: string;
-  staffId: string;
-  date: string;
-  startTime: string;
-  endTime: string;
+    id: string
+    staffId: string
+    date: string
+    startTime: string
+    endTime: string
+}
+
+export interface ShiftTemplateDay {
+    day: string
+    times: ShiftTemplateTime[]
+}
+
+export interface ShiftTemplateTime {
+    startTime: string
+    endTime: string
+}
+
+export interface ShiftTemplate {
+    id: string
+    userId: string
+    configPerDay: [key: string, value: ShiftTemplateDay][]
 }
