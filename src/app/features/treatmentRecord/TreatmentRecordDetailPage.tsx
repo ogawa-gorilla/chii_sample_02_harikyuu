d07b5942519ae4dc65a1894cec147632b94e4e8a
Gosuke <ogawa.gousuke@gmail.com>
2025-06-25 19:26:00 +0900

    詳細画面からの編集に対応

--- src/app/features/treatmentRecord/TreatmentRecordDetailPage.tsx
+++ src/app/features/treatmentRecord/TreatmentRecordDetailPage.tsx
@@ -1,5 +1,11 @@
 import { useAppDispatch, useAppSelector } from '@/app/hooks'
-import { popPage } from '@/app/store/navigationSlice'
+import { popPage, pushPage } from '@/app/store/navigationSlice'
+import {
+    setRecordDraft,
+    setRecordOnView,
+} from '@/app/store/treatmentRecordSlice'
+import { Page } from '@/app/types/Page'
+import { TreatmentRecord } from '@/app/types/treatmentRecord'
 import { useState } from 'react'
 import { Button, Card, Col, Container, Image, Row } from 'react-bootstrap'
 import ImageModal from './components/ImageModal'
@@ -8,20 +14,6 @@ const TreatmentRecordDetail = () => {
     const dispatch = useAppDispatch()
     const [showImageModal, setShowImageModal] = useState(false)
     const [selectedImageUrl, setSelectedImageUrl] = useState('')
-
-    const handleBack = () => {
-        dispatch(popPage())
-    }
-
-    const handleImageClick = (imageUrl: string) => {
-        setSelectedImageUrl(imageUrl)
-        setShowImageModal(true)
-    }
-
-    const handleCloseImageModal = () => {
-        setShowImageModal(false)
-        setSelectedImageUrl('')
-    }
 
     const recordIdOnView = useAppSelector(
         (state) => state.treatmentRecords.recordIdOnView
@@ -35,6 +27,30 @@ const TreatmentRecordDetail = () => {
     const staff = useAppSelector((state) =>
         state.user.users.find((user) => user.id === record?.staffId)
     )
+
+    const handleBack = () => {
+        dispatch(popPage())
+    }
+
+    const handleEdit = () => {
+        if (!record) return
+        const newDraft: TreatmentRecord = {
+            ...record,
+        }
+        dispatch(setRecordDraft(newDraft))
+        dispatch(setRecordOnView(newDraft.id))
+        dispatch(pushPage(Page.TREATMENT_RECORD_EDIT))
+    }
+
+    const handleImageClick = (imageUrl: string) => {
+        setSelectedImageUrl(imageUrl)
+        setShowImageModal(true)
+    }
+
+    const handleCloseImageModal = () => {
+        setShowImageModal(false)
+        setSelectedImageUrl('')
+    }
 
     return (
         record && (
@@ -88,7 +104,10 @@ const TreatmentRecordDetail = () => {
                             </>
                         )}
                     </Card.Body>
-                    <Card.Footer>
+                    <Card.Footer className="d-flex justify-content-end gap-2">
+                        <Button variant="primary" onClick={handleEdit}>
+                            編集
+                        </Button>
                         <Button variant="secondary" onClick={handleBack}>
                             戻る
                         </Button>
