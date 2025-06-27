7ac306825754e68e76daa4d9155c18ece60c841b
Gosuke <ogawa.gousuke@gmail.com>
2025-06-27 17:15:42 +0900

    バグ直した

--- src/app/features/reservation/components/DatePickerCell.tsx
+++ src/app/features/reservation/components/DatePickerCell.tsx
@@ -1,62 +1,92 @@
-import { Reservation } from "@/app/types/reservation";
-import { Shift } from "@/app/types/shift";
-import { isReservedAt } from "@/app/utils/reservationUtils";
-import { filterShiftsAtHour } from "@/app/utils/shiftUtils";
-import dayjs from "dayjs";
+import { Reservation } from '@/app/types/reservation'
+import { Shift } from '@/app/types/shift'
+import { isReservedAt } from '@/app/utils/reservationUtils'
+import { filterShiftsAtHour } from '@/app/utils/shiftUtils'
+import dayjs from 'dayjs'
 
 interface DatePickerCellProps {
-  day: dayjs.Dayjs;
-  hour: number;
-  allReservations: Reservation[];
-  allShifts: Shift[];
-  currentSelection: {
-    day: dayjs.Dayjs;
-    hour: number;
-  }
-  reservationId: string;
-  onCellClick?: (date: string, hour: number, reservations: Reservation[]) => void;
+    day: dayjs.Dayjs
+    hour: number
+    allReservations: Reservation[]
+    allShifts: Shift[]
+    currentSelection: {
+        day: dayjs.Dayjs
+        hour: number
+    }
+    reservationId: string
+    onCellClick?: (
+        date: string,
+        hour: number,
+        reservations: Reservation[]
+    ) => void
 }
 
 const getShifts = (allShifts: Shift[], day: dayjs.Dayjs, hour: number) => {
-  return filterShiftsAtHour(allShifts, day.format('YYYY-MM-DD'), hour);
+    return filterShiftsAtHour(allShifts, day.format('YYYY-MM-DD'), hour)
 }
 
-export default function DatePickerCell({ day, hour, allReservations, allShifts, currentSelection, reservationId, onCellClick }: DatePickerCellProps) {
-
-  
-  const getReservations = (reservations: Reservation[], day: dayjs.Dayjs, hour: number) => {
-    return reservations.filter(reservation => isReservedAt(reservation, day.format('YYYY-MM-DD'), hour));
-  }
-
-  const hasReservations = getReservations(allReservations, day, hour).length > 0;
-  const hasShifts = getShifts(allShifts, day, hour).length > 0;
-
-  const handleClick = () => {
-    if (onCellClick) {
-      onCellClick(day.format('YYYY-MM-DD'), hour, getReservations(allReservations, day, hour));
+export default function DatePickerCell({
+    day,
+    hour,
+    allReservations,
+    allShifts,
+    currentSelection,
+    reservationId,
+    onCellClick,
+}: DatePickerCellProps) {
+    const getReservations = (
+        reservations: Reservation[],
+        day: dayjs.Dayjs,
+        hour: number
+    ) => {
+        return reservations.filter((reservation) =>
+            isReservedAt(reservation, day.format('YYYY-MM-DD'), hour)
+        )
     }
-  }
 
-  const renderContent = (hasReservations: boolean, hasShifts: boolean) => {
-    if (currentSelection.day.isSame(day, 'day') && currentSelection.hour === hour) {
-      return <div className="align-items-center current-selection">選択中</div>;
-    }
-    if (!hasShifts) {
-      return <div>休</div>;
-    }
-    if (hasReservations) {
-      return <div className="align-items-center">×</div>;
-    }
-    return <div>空</div>;
-  }
+    const hasReservations =
+        getReservations(allReservations, day, hour).filter(
+            (r) => r.id !== reservationId
+        ).length > 0
+    const hasShifts = getShifts(allShifts, day, hour).length > 0
 
-  return (
-    <td 
-      key={`${day.format()}-${hour}`} 
-      onClick={handleClick}
-      className={hasShifts && !hasReservations ? '' : 'not-available'}
-    >
-      {renderContent(hasReservations, hasShifts)}
-    </td>
-  );
+    const handleClick = () => {
+        if (onCellClick) {
+            onCellClick(
+                day.format('YYYY-MM-DD'),
+                hour,
+                getReservations(allReservations, day, hour)
+            )
+        }
+    }
+
+    const renderContent = (hasReservations: boolean, hasShifts: boolean) => {
+        if (
+            currentSelection.day.isSame(day, 'day') &&
+            currentSelection.hour === hour
+        ) {
+            return (
+                <div className="align-items-center current-selection">
+                    選択中
+                </div>
+            )
+        }
+        if (!hasShifts) {
+            return <div>休</div>
+        }
+        if (hasReservations) {
+            return <div className="align-items-center">×</div>
+        }
+        return <div>空</div>
+    }
+
+    return (
+        <td
+            key={`${day.format()}-${hour}`}
+            onClick={handleClick}
+            className={hasShifts && !hasReservations ? '' : 'not-available'}
+        >
+            {renderContent(hasReservations, hasShifts)}
+        </td>
+    )
 }
--- src/app/features/reservation/components/DatePickerModal.tsx
+++ src/app/features/reservation/components/DatePickerModal.tsx
@@ -1,57 +1,90 @@
-import Calendar from "@/app/components/calendar";
-import { useAppSelector } from "@/app/hooks";
-import { Reservation } from "@/app/types/reservation";
-import { User } from "@/app/types/user";
-import { filterReservedAt } from "@/app/utils/reservationUtils";
-import { filterShiftsAtHour } from "@/app/utils/shiftUtils";
-import dayjs from "dayjs";
-import { Button, Modal } from "react-bootstrap";
-import DatePickerCell from "./DatePickerCell";
+import Calendar from '@/app/components/calendar'
+import { useAppSelector } from '@/app/hooks'
+import { Reservation } from '@/app/types/reservation'
+import { User } from '@/app/types/user'
+import { filterReservedAt } from '@/app/utils/reservationUtils'
+import { filterShiftsAtHour } from '@/app/utils/shiftUtils'
+import dayjs from 'dayjs'
+import { Button, Modal } from 'react-bootstrap'
+import DatePickerCell from './DatePickerCell'
 
 interface DatePickerModalProps {
-  show: boolean;
-  onHide: () => void;
-  staff: User;
-  currentSelection: {
-    day: dayjs.Dayjs;
-    hour: number;
-  }
-  reservationId: string | null;
-  onDateSelected: (date: string, hour: number) => void;
+    show: boolean
+    onHide: () => void
+    staff: User
+    currentSelection: {
+        day: dayjs.Dayjs
+        hour: number
+    }
+    reservationId: string | null
+    onDateSelected: (date: string, hour: number) => void
 }
 
-export default function DatePickerModal({ show, onHide, staff, currentSelection, reservationId, onDateSelected }: DatePickerModalProps) {
+export default function DatePickerModal({
+    show,
+    onHide,
+    staff,
+    currentSelection,
+    reservationId,
+    onDateSelected,
+}: DatePickerModalProps) {
+    const allReservations = useAppSelector(
+        (state) => state.reservation.reservations
+    ).filter((reservation) => reservation.staff.id === staff.id)
+    const allShifts = useAppSelector((state) => state.shift.shifts).filter(
+        (shift) => shift.staffId === staff.id
+    )
 
-  const allReservations = useAppSelector((state) => state.reservation.reservations).filter(reservation => reservation.staff.id === staff.id);
-  const allShifts = useAppSelector((state) => state.shift.shifts).filter(shift => shift.staffId === staff.id);
+    const handleCellClick = (
+        date: string,
+        hour: number,
+        reservations: Reservation[]
+    ) => {
+        if (
+            filterShiftsAtHour(allShifts, date, hour).length === 0 ||
+            filterReservedAt(allReservations, date, hour).filter(
+                (r) => r.id !== reservationId
+            ).length > 0
+        ) {
+            return
+        }
 
-  const handleCellClick = (date: string, hour: number, reservations: Reservation[]) => {
-    if (filterShiftsAtHour(allShifts, date, hour).length === 0 || filterReservedAt(allReservations, date, hour).length > 0) {
-      return;
+        onDateSelected(date, hour)
     }
 
-    onDateSelected(date, hour);
-  }
-
-  return (
-      <Modal show={show} onHide={onHide} size="lg" centered>
-        <style>
-          {`
+    return (
+        <Modal show={show} onHide={onHide} size="lg" centered>
+            <style>
+                {`
             .current-selection {
               background-color: #007bff;
               color: white;
             }
           `}
-        </style>
-        <Modal.Header closeButton>
-          <Modal.Title>予約日時選択({staff.name})</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="p-2">
-          <Calendar cellComponent={DatePickerCell} cellProps={{ allReservations, allShifts, currentSelection: { day: dayjs(currentSelection.day), hour: currentSelection.hour }, reservationId: reservationId ?? '', onCellClick: handleCellClick }} />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={onHide}>閉じる</Button>
-        </Modal.Footer>
-      </Modal>
-  );
+            </style>
+            <Modal.Header closeButton>
+                <Modal.Title>予約日時選択({staff.name})</Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="p-2">
+                <Calendar
+                    cellComponent={DatePickerCell}
+                    cellProps={{
+                        allReservations,
+                        allShifts,
+                        currentSelection: {
+                            day: dayjs(currentSelection.day),
+                            hour: currentSelection.hour,
+                        },
+                        reservationId: reservationId ?? '',
+                        onCellClick: handleCellClick,
+                    }}
+                />
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={onHide}>
+                    閉じる
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    )
 }
