5afee488ec11bb44319c86c7f6a4710bf8e7b37e
Gosuke <ogawa.gousuke@gmail.com>
2025-06-27 16:17:08 +0900

    編集時のログ

--- src/app/features/treatmentRecord/TreatmentRecordEditPage.tsx
+++ src/app/features/treatmentRecord/TreatmentRecordEditPage.tsx
@@ -1,16 +1,19 @@
 import { useAppDispatch, useAppSelector } from '@/app/hooks'
 import { popPage, setCurrentPage } from '@/app/store/navigationSlice'
-import { updateRecord } from '@/app/store/treatmentRecordSlice'
+import { setRecordOnView } from '@/app/store/treatmentRecordSlice'
 import { Page } from '@/app/types/Page'
 import TreatmentRecordEditForm from './components/TreatmentRecordEditForm'
+import { useTreatmentForm } from './hooks/useTreatmentForm'
 
 const TreatmentRecordEditPage = () => {
     const dispatch = useAppDispatch()
 
     const draft = useAppSelector((state) => state.treatmentRecords.recordDraft)
+    const { updateRecordEntry } = useTreatmentForm()
 
     const handleSubmit = () => {
-        dispatch(updateRecord(draft))
+        updateRecordEntry(draft)
+        dispatch(setRecordOnView(draft.id))
         dispatch(setCurrentPage(Page.TREATMENT_RECORD_DETAIL))
     }
 
--- src/app/features/treatmentRecord/hooks/useTreatmentForm.tsx
+++ src/app/features/treatmentRecord/hooks/useTreatmentForm.tsx
@@ -2,6 +2,7 @@ import { useAppDispatch, useAppSelector } from '@/app/hooks'
 import { addEditLog } from '@/app/store/editLogSlice'
 import {
     createRecord,
+    updateRecord,
     updateRecordDraft,
 } from '@/app/store/treatmentRecordSlice'
 import { EditLog, EditLogTag, EditLogTarget } from '@/app/types/EditLog'
@@ -24,11 +25,54 @@ const createCreateLog = (record: TreatmentRecord, user: User): EditLog => {
     }
 }
 
+const areImageListsEqual = (list1: string[], list2: string[]) => {
+    if (list1.length !== list2.length) {
+        return false
+    }
+    return list1.every((img, index) => img === list2[index])
+}
+
+const createUpdateLog = (
+    originalRecord: TreatmentRecord,
+    updatedRecord: TreatmentRecord,
+    user: User
+): EditLog => {
+    const log = {
+        id: v4(),
+        editTarget: EditLogTarget.TREATMENT_RECORD,
+        user: user,
+        editedAt: dayjs().toISOString(),
+        edits: [] as string[],
+        tags: [EditLogTag.EDIT],
+    }
+
+    const edits: string[] = []
+
+    if (originalRecord.content !== updatedRecord.content) {
+        edits.push(
+            `施術内容: ${originalRecord.content} -> ${updatedRecord.content}`
+        )
+    }
+
+    if (
+        !areImageListsEqual(
+            originalRecord.attached_images,
+            updatedRecord.attached_images
+        )
+    ) {
+        edits.push(`添付画像に変更`)
+    }
+
+    log.edits = edits
+    return log
+}
+
 export const useTreatmentForm = () => {
     const dispatch = useAppDispatch()
 
     const draft = useAppSelector((state) => state.treatmentRecords.recordDraft)
     const loginUser = useAppSelector((state) => state.login.user)
+    const records = useAppSelector((state) => state.treatmentRecords.records)
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target
@@ -65,11 +109,25 @@ export const useTreatmentForm = () => {
         [dispatch, loginUser]
     )
 
+    const updateRecordEntry = useCallback(
+        (record: TreatmentRecord) => {
+            const originalRecord = records.find((r) => r.id === record.id)!
+            const log = createUpdateLog(originalRecord, record, loginUser!)
+            dispatch(updateRecord(record))
+            if (log.edits.length > 0) {
+                log.backup = JSON.stringify(originalRecord)
+                dispatch(addEditLog(log))
+            }
+        },
+        [dispatch, loginUser, records]
+    )
+
     return {
         handleChange,
         handleFileChange,
         handleDeleteImage,
 
         createRecordEntry,
+        updateRecordEntry,
     }
 }
